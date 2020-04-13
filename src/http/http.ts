ecdab8415507368b9e8e10d083e6b4722f609fe2
Enes Usta <enesusta@email.com>
2020-04-13 16:22:06 +0300

    Initial commit

---
+++ src/http/http.ts
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+class SingletonClient {
+    constructor() {
+
+    }
+    get(url: string) {
+        return axios.get(url);
+    }
+    post(url: string) {
+        return axios.post(url);
+    }
+}
+
+const http: SingletonClient = new SingletonClient();
+Object.freeze(http);
+
+export default http;

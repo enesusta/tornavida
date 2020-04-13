d7045af50639b5da859a8bc15df89970c19269e9
Enes Usta <enesusta@email.com>
2020-04-13 17:24:22 +0300

    Webpack and babel configurations completed

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,52 @@
-const path = require('path');
+/* global __dirname, require, module*/
 
-module.exports = {
-    entry: path.join(__dirname, '/src/index.ts'),
-    output: {
-        filename: 'bundle.js'
-    },
-    externals: ['axios'],
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                loader: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ]
-    },
-    resolve: {
-        modules: [path.resolve(__dirname, '/src', 'node_modules/')],
-        descriptionFiles: ['package.json'],
-        extensions: [".tsx", ".ts", ".js"]
-    },
+const webpack = require('webpack');
+const path = require('path');
+const env = require('yargs').argv.env; // use --env with webpack 2
+const pkg = require('./package.json');
+
+let libraryName = pkg.name;
+
+let outputFile, mode;
+
+if (env === 'build') {
+  mode = 'production';
+  outputFile = libraryName + '.min.js';
+} else {
+  mode = 'development';
+  outputFile = libraryName + '.js';
 }
+
+const config = {
+  mode: mode,
+  entry: __dirname + '/src/index.js',
+  devtool: 'inline-source-map',
+  output: {
+    path: __dirname + '/dist',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true,
+    globalObject: "typeof self !== 'undefined' ? self : this"
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'eslint-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve('./node_modules'), path.resolve('./src')],
+    extensions: ['.json', '.js']
+  }
+};
+
+module.exports = config;

69267861fac48582dde7679f6fabc36b52c37c17
Enes Usta <enesusta@protonmail.com>
2022-07-25 00:28:17 +0300

    feat: add native md5 function

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
-import typescript from 'rollup-plugin-typescript2'
-import commonjs from '@rollup/plugin-commonjs'
-import resolve from '@rollup/plugin-node-resolve'
-import {terser} from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import { terser } from "rollup-plugin-terser";
 
-import pkg from './package.json'
+import pkg from "./package.json";
 
 export default [
   {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     output: [
       {
         file: pkg.main,
-        format: 'cjs',
-        exports: 'named',
-        sourcemap: true
+        format: "cjs",
+        exports: "named",
+        sourcemap: true,
       },
       {
         file: pkg.module,
-        format: 'es',
-        exports: 'named',
-        sourcemap: true
-      }
+        format: "es",
+        exports: "named",
+        sourcemap: true,
+      },
     ],
     plugins: [
       resolve(),
@@ -28,113 +28,37 @@ export default [
         rollupCommonJSResolveHack: true,
         clean: true,
         declaration: true,
-        declarationDir: 'dist'
-      }),
-      commonjs(),
-      terser()
-    ]
-  },
-  {
-    input: 'src/util/index.ts',
-    output: [
-      {
-        dir: 'util',
-        format: 'es'
-      }
-    ],
-    plugins: [
-      typescript({
-        rollupCommonJSResolveHack: true,
-        clean: true,
-        declaration: true,
-        declarationDir: 'util'
-      }),
-      terser()
-    ]
-  },
-  {
-    input: 'src/util/index.ts',
-    output: [
-      {
-        dir: 'util/cjs',
-        format: 'cjs'
-      }
-    ],
-    plugins: [
-      resolve(),
-      typescript({
-        rollupCommonJSResolveHack: true,
-        clean: true,
-        declaration: true,
-        declarationDir: 'util/cjs'
-      }),
-      commonjs(),
-      terser()
-    ]
-  },
-  {
-    input: 'src/math/index.ts',
-    output: [
-      {
-        dir: 'math',
-        format: 'es'
-      }
-    ],
-    plugins: [
-      typescript({
-        rollupCommonJSResolveHack: true,
-        clean: true,
-        declaration: true,
-        declarationDir: 'math'
-      }),
-      terser()
-    ],
-  },
-  {
-    input: 'src/math/index.ts',
-    output: [
-      {
-        dir: 'math/cjs',
-        format: 'cjs'
-      }
-    ],
-    plugins: [
-      resolve(),
-      typescript({
-        rollupCommonJSResolveHack: true,
-        clean: true,
-        declaration: true,
-        declarationDir: 'math/cjs'
+        declarationDir: "dist",
       }),
       commonjs(),
       terser(),
     ],
   },
   {
-    input: 'src/text/index.ts',
+    input: "src/util/index.ts",
     output: [
       {
-        dir: 'text',
-        format: 'es'
-      }
+        dir: "util",
+        format: "es",
+      },
     ],
     plugins: [
       typescript({
         rollupCommonJSResolveHack: true,
         clean: true,
         declaration: true,
-        declarationDir: 'text'
+        declarationDir: "util",
       }),
-      terser()
-    ]
+      terser(),
+    ],
   },
   {
-    input: 'src/text/index.ts',
+    input: "src/util/index.ts",
     output: [
       {
-        dir: 'text/cjs',
-        format: 'cjs'
-      }
+        dir: "util/cjs",
+        format: "cjs",
+      },
     ],
     plugins: [
       resolve(),
@@ -142,10 +66,124 @@ export default [
         rollupCommonJSResolveHack: true,
         clean: true,
         declaration: true,
-        declarationDir: 'text/cjs'
+        declarationDir: "util/cjs",
       }),
       commonjs(),
       terser(),
-    ]
+    ],
+  },
+  {
+    input: "src/math/index.ts",
+    output: [
+      {
+        dir: "math",
+        format: "es",
+      },
+    ],
+    plugins: [
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        declaration: true,
+        declarationDir: "math",
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "src/math/index.ts",
+    output: [
+      {
+        dir: "math/cjs",
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        declaration: true,
+        declarationDir: "math/cjs",
+      }),
+      commonjs(),
+      terser(),
+    ],
+  },
+  {
+    input: "src/text/index.ts",
+    output: [
+      {
+        dir: "text",
+        format: "es",
+      },
+    ],
+    plugins: [
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        declaration: true,
+        declarationDir: "text",
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "src/text/index.ts",
+    output: [
+      {
+        dir: "text/cjs",
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        declaration: true,
+        declarationDir: "text/cjs",
+      }),
+      commonjs(),
+      terser(),
+    ],
+  },
+  {
+    input: "src/hash/index.ts",
+    output: [
+      {
+        dir: "hash",
+        format: "es",
+      },
+    ],
+    plugins: [
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        declaration: true,
+        declarationDir: "hash",
+      }),
+      terser(),
+    ],
+  },
+  {
+    input: "src/hash/index.ts",
+    output: [
+      {
+        dir: "hash/cjs",
+        format: "cjs",
+      },
+    ],
+    plugins: [
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        declaration: true,
+        declarationDir: "hash/cjs",
+      }),
+      commonjs(),
+      terser(),
+    ],
   },
 ];
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import fieldMax from './math/fieldMax';
-import capitalizeWithTurkish from './text/capitalizeWithTurkish';
-import isArraysEqual from './util/isArraysEqual';
-import isOdd from './util/isOdd';
-import sortAscByField from './util/sortAscByField';
-import sortDescByField from './util/sortDescByField';
-import isPrimitiveArraysEqual from './util/isPrimitiveArraysEqual';
+import fieldMax from "./math/fieldMax";
+import capitalizeWithTurkish from "./text/capitalizeWithTurkish";
+import isArraysEqual from "./util/isArraysEqual";
+import isOdd from "./util/isOdd";
+import sortAscByField from "./util/sortAscByField";
+import sortDescByField from "./util/sortDescByField";
+import isPrimitiveArraysEqual from "./util/isPrimitiveArraysEqual";
+import md5 from "./hash/md5";
 
 export default {
-    findLongestLengthFieldOfArray: fieldMax,
-    capitalizeWithTurkish,
-    isArraysEqual,
-    isOdd,
-    isPrimitiveArraysEqual,
-    sortAscByField,
-    sortDescByField
+  findLongestLengthFieldOfArray: fieldMax,
+  capitalizeWithTurkish,
+  isArraysEqual,
+  isOdd,
+  isPrimitiveArraysEqual,
+  sortAscByField,
+  sortDescByField,
+  md5,
 };
